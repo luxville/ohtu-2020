87f3ec2e383f2cb5d82b790b0232ac206365bc29
Ville Luostarinen <ville.luostarinen@helsinki.fi>
2020-11-21 19:54:21 +0200

    Tehtävä 4.3

---
+++ viikko2/Verkkokauppa1/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -0,0 +1,76 @@
+package ohtu.verkkokauppa;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class KauppaTest {
+
+    Pankki pankki;
+    Viitegeneraattori viite;
+    Varasto varasto;
+    Kauppa k;
+
+    @Before
+    public void setUp() {
+        pankki = mock(Pankki.class);
+        viite = mock(Viitegeneraattori.class);
+        varasto = mock(Varasto.class);
+        k = new Kauppa(varasto, pankki, viite);
+        when(viite.uusi()).thenReturn(42);
+        when(varasto.saldo(1)).thenReturn(10); 
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        when(varasto.saldo(2)).thenReturn(13); 
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "piimä", 3));
+        when(varasto.saldo(3)).thenReturn(0);
+        when(varasto.haeTuote(3)).thenReturn(new Tuote(3, "Tesla", 85499));
+        k.aloitaAsiointi();
+    }
+
+    @Test
+    public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaan() {
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+        verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(),anyInt());   
+        // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
+    }
+
+    @Test
+    public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaanOikeillaParametreilla() {
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("pekka", "12345");
+
+        verify(pankki).tilisiirto("pekka", viite.uusi(), "12345", "33333-44455", 5);   
+    }
+
+    @Test
+    public void ostoksenPaaytyttyaEriTuotteidenOstonJalkeenPankinMetodiaTilisiirtoKutsutaanOikeillaParametreilla() {
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.lisaaKoriin(2);     // ostetaan tuotetta numero 2 eli piimää
+        k.tilimaksu("pekka", "12345");
+
+        verify(pankki).tilisiirto("pekka", viite.uusi(), "12345", "33333-44455", 8);   
+    }
+
+    @Test
+    public void ostoksenPaaytyttyaSamojenTuotteidenOstonJalkeenPankinMetodiaTilisiirtoKutsutaanOikeillaParametreilla() {
+        k.lisaaKoriin(2);
+        k.lisaaKoriin(2);
+        k.tilimaksu("pekka", "12345");
+
+        verify(pankki).tilisiirto("pekka", viite.uusi(), "12345", "33333-44455", 6);   
+    }
+
+    @Test
+    public void ostoksenPaaytyttyaJodToinenTuoteOnLoppunutMaksetaanVainVarastossaOlleista() {
+        k.lisaaKoriin(2);
+        k.lisaaKoriin(3);
+        k.tilimaksu("pekka", "12345");
+
+        verify(pankki).tilisiirto("pekka", viite.uusi(), "12345", "33333-44455", 3);   
+    }
+}
+
